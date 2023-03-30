c360f7662dd8a83d48df456c4ac0b1025550aecd
oussama <oussama.ennadafy@gmail.com>
2023-03-30 21:20:01 +0000

    update

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
+const fs = require('fs')
 const express = require('express');
 const app = express()
 const port = 8000
 
+// read json files
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'))
 
-app.get('/', (req, res, next) =>
+// parse the body 
+app.use(express.json());
+
+// routes
+app.get('/api/v1/tours', (req, res) =>
 {
- res.status(200).send('<h1>you try to get from express server</h1>')
+ res.json({
+  status: "success",
+  results: tours.length,
+  data: {
+   tours
+  }
+ })
 })
 
-app.post('/', (req, res, next) =>
+app.post('/api/v1/tours', (req, res) =>
 {
- res.status(200).send('<h1>you try to post to express server</h1>')
+ const item = req.body
+ fs.readFile(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8', (err, data) =>
+ {
+  if (err) return res.status(400).json({ error: "something went wrong" })
+  const dataObj = JSON.parse(data)
+  dataObj.push(item)
+  const newTour = JSON.stringify(dataObj)
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, newTour, () =>
+  {
+   res.status(201).json({
+    status: "success",
+    data: {
+     tour: newTour
+    }
+   })
+  })
+ })
 })
 
 
