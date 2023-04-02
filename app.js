83de0f30cfc7f49ee608f9cd099cc75431316bde
oussama <oussama.ennadafy@gmail.com>
2023-04-02 14:00:05 +0000

    remove static data

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const toursRouter = require('./routes/tours')
 // parse the body 
 app.use(express.json());
 
-const users = [
-  {
-    name: "oussama",
-    age: 20
-  }
-]
-
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/tours', toursRouter)
 
