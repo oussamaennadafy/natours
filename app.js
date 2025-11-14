700fd8fc3510322f943b05c86781b5c96674fcda
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-11-14 12:27:52 +0100

    feat(rateLimit): add rate limiting middleware to API routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const authRouter = require("./routes/authRoutes");
@@ -12,8 +13,16 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.static(`/${__dirname}/public`));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "too many requests from this ip, please try again in an hour!",
+});
+
+app.use("/api/*", limiter);
 app.use(express.json());
+app.use(express.static(`/${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
