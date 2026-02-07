c1d1628e7794145d12b435a9d70643e2c4a3311f
oussama ennadafy <oussama.ennadafy@gmail.com>
2026-02-07 21:47:38 +0100

    finally its working perfectly, stripe webhook is done in a clean way

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,16 +60,6 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
-  console.log({
-    "-------------------------------------session-------------------------------------":
-      session,
-    "-------------------------------------tour-------------------------------------":
-      tour,
-    "-------------------------------------user-------------------------------------":
-      user,
-    "-------------------------------------price-------------------------------------":
-      price,
-  });
   await Booking.create({
     tour,
     user,
